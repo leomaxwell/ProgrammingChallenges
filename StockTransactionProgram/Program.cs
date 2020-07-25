e98fbb94364d5f4a7a9e3b71d1ceea5fe670d658
Leo Maxwell <leonardo.maxwell@comaxict.com>
2020-07-25 17:39:03 +0000

    Complete implementation of StockTransactionProgram exercise

---
+++ StockTransactionProgram/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace StockTransactionProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable Declarations
+            int totalSharesPurchased = 100;
+            decimal purchasePricePerShare = 32.87m;
+            decimal salePricePerShare = 33.92m;
+            decimal brokerRate = (2.00m / 100.00m);
+
+
+            decimal stockPurchaseAmount;
+            decimal commissionOnPurchase;
+            decimal stockSaleAmount;
+            decimal commissionOnSale;
+            decimal netEarningsOnTrade;
+
+            //Processing
+            stockPurchaseAmount = (totalSharesPurchased * purchasePricePerShare);
+            commissionOnPurchase = (stockPurchaseAmount * brokerRate);
+            stockSaleAmount = (totalSharesPurchased * salePricePerShare);
+            commissionOnSale = (stockSaleAmount * brokerRate);
+            netEarningsOnTrade = stockSaleAmount - (stockPurchaseAmount + commissionOnPurchase + commissionOnSale);
+
+
+            // Message to the user
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("This program calculates and provides");
+            Console.WriteLine("the Net Transaction Earnings on stocks");
+            Console.WriteLine("traded by Joe in June, 2020.");
+            Console.WriteLine("------------------------------------------");
+
+            Console.WriteLine("Stock Purchase Amount: \t" + stockPurchaseAmount.ToString("C").PadLeft(9, ' '));
+            Console.WriteLine("Commission on Purchase: " + commissionOnPurchase.ToString("C").PadLeft(9, ' '));
+            Console.WriteLine("Stock Sale Earnings: \t" + stockSaleAmount.ToString("C").PadLeft(9, ' '));
+            Console.WriteLine("Commission on Sale: \t" + commissionOnSale.ToString("C").PadLeft(9, ' '));
+
+            Console.WriteLine("\nNET EARNINGS ON TRADE: \t" + netEarningsOnTrade.ToString("C").PadLeft(9, ' '));
+
+            Console.WriteLine("");
+            Console.WriteLine("Thank You!");
+
+            Console.ReadLine();
+        }
+    }
+}
