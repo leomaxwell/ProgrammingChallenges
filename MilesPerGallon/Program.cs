2c1026b189c88ff35f10203dca3261acdae0021d
Leo Maxwell <leonardo.maxwell@comaxict.com>
2020-07-25 15:07:55 +0000

    Initial Setup of Programming Challenges Solutions with MilesPerGallon Project

---
+++ MilesPerGallon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MilesPerGallon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
