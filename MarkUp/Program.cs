ae9f339a75e16633fe391e22b77f24e53ad97a52
Leo Maxwell <leonardo.maxwell@comaxict.com>
2020-08-01 15:14:59 +0000

    Implementation of MarkUp progect.

---
+++ MarkUp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MarkUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal wholesaleCost;
+            decimal markupPercentage;
+            decimal retailPrice;
+
+            string input = ""; // Temporary holder of user input
+
+            // Message to the user
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("This program calculates the retail price");
+            Console.WriteLine("of an item given its wholesale cost");
+            Console.WriteLine("and markup percentage.");
+            Console.WriteLine("------------------------------------------");
+
+            Console.Write("Wholesale Cost: ");
+            input = Console.ReadLine();
+            wholesaleCost = Convert.ToDecimal(input);
+
+            Console.Write("Markup Percentage: ");
+            input = Console.ReadLine();
+            markupPercentage = Convert.ToDecimal(input);
+
+            retailPrice = CalculateRetailPrice(wholesaleCost, markupPercentage);
+
+            //Outputting result to user
+            Console.WriteLine("\nRetail Price: $" + retailPrice);
+
+            Console.WriteLine("");
+            Console.WriteLine("Thank You!");
+
+            Console.ReadLine();
+
+        }
+
+        /// <summary>
+        /// This method calculates the retail price given the cost and markup percent.
+        /// </summary>
+        /// <param name="cost">The wholesale cost of the item.</param>
+        /// <param name="percent">The intended markup percent.</param>
+        /// <returns>The retails price of the item.</returns>
+        public static decimal CalculateRetailPrice(decimal cost, decimal percent)
+        {
+            decimal markup = (percent / 100);
+
+            decimal retailPrice = cost + (cost * markup);
+
+            return retailPrice;
+        }
+    }
+}
